e2f8015be0a51da659079acba3913351f37ef696
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-01 16:01:38 +0200

    add navigation and improve routing for Contacts and Products pages

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,43 @@
 export default function Homepage() {
     return (
         <>
+            <header>
+                <nav
+                    class="navbar navbar-expand-sm navbar-light bg-light"
+                >
+                    <div class="container">
+                        <a class="navbar-brand" href="#"><i class="fa fa-long-arrow-down" aria-hidden="true"></i>LOGO</a>
+                        <button
+                            class="navbar-toggler d-lg-none"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapsibleNavId"
+                            aria-controls="collapsibleNavId"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="collapsibleNavId">
+                            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="#" aria-current="page"
+                                    >Homepage
+                                        <span class="visually-hidden">(current)</span></a
+                                    >
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Products</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Contacts</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
 
+            </header >
         </>
     )
 }
