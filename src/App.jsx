664d35667330e71a783214a62c1719b722a2146f
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-01 15:36:48 +0200

    add routing and create basic page components for Homepage, Contacts, and Products

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router"
+import Homepage from "./pages/Homepage"
+import Contacts from "./pages/Contacts"
+import Products from "./pages/Products"
+
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Homepage} />
+          <Route Component={Contacts} />
+          <Route Component={Products} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
